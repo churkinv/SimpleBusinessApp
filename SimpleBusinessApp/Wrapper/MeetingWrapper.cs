35bb02fd3656f8fe1c9f5ddb1588d233fd7c5ab5
churkinv <churkinv@gmail.com>
2017-11-09 08:06:23 +0200

    MeetingWrapper added

---
+++ SimpleBusinessApp/Wrapper/MeetingWrapper.cs
@@ -0,0 +1,36 @@
+﻿using SimpleBusinessApp.Model;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SimpleBusinessApp.Wrapper
+{
+    public class MeetingWrapper : ModelWrapper<Meeting>
+    {
+        public MeetingWrapper(Meeting model) : base(model)
+        {
+        }
+
+        public int Id { get { return Model.Id; } }
+
+        public string Titel
+        {
+            get { return GetValue<string>(); }
+            set { SetValue(value); }
+        }
+
+        public DateTime DateFrom
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+
+        public DateTime DateTo
+        {
+            get { return GetValue<DateTime>(); }
+            set { SetValue(value); }
+        }
+    }
+}
